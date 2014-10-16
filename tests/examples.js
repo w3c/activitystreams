6e02621a4b2faf939e7f42ba244bf7c081ae2ff1
elf Pavlik <perpetual-tripper@wwelves.org>
2014-10-17 01:17:12 +0200

    using local context + updated test to new examples

--- tests/examples.js
+++ tests/examples.js
@@ -3,7 +3,7 @@ var JSONLD = require('jsonld');
 var promises = JSONLD.promises();
 var expect = require('chai').expect;
 
-describe('Basic Activity', function(){
+describe('Minimal Activity', function(){
   it('should match expected RDF', function(done){
     var example = helper.getExample('example-1');
     var expected = helper.getFixture('example-1');
@@ -20,10 +20,27 @@ describe('Basic Activity', function(){
   });
 });
 
-describe('Extended Activity', function(){
+describe('Basic Activity', function(){
   it('should match expected RDF', function(done){
     var example = helper.getExample('example-2');
     var expected = helper.getFixture('example-2');
+
+    promises.toRDF(example, {format: 'application/nquads'})
+      .then(function(nquads){
+        expect(nquads).to.equal(expected);
+        done();
+      })
+      .catch(function(err){
+        console.log(err);
+        done();
+      });
+  });
+});
+
+describe('Extended Activity', function(){
+  it('should match expected RDF', function(done){
+    var example = helper.getExample('example-3');
+    var expected = helper.getFixture('example-3');
 
     promises.toRDF(example, {format: 'application/nquads'})
       .then(function(nquads){
--- tests/helper.js
+++ tests/helper.js
@@ -5,14 +5,14 @@ var $ = require('cheerio');
 var examples = {};
 var fixtures = {};
 
-var CONTEXT_URI = 'http://asjsonld.mybluemix.net/';
+var CONTEXT = JSON.parse(fs.readFileSync('./activitystreams2-context.jsonld').toString())["@context"];
 
 var doc = $.load(fs.readFileSync('./activitystreams2.html').toString());
 
 var getExample = function(name) {
   if (!examples[name]) {
     examples[name] = JSON.parse(doc('#' + name + ' pre.example').text());
-    examples[name]['@context'] = CONTEXT_URI;
+    examples[name]['@context'] = CONTEXT;
   }
   return examples[name];
 };
